26769dca25f5d4c53fd887f07f378e0bee557e9a
Fede2000 <fede.sarrocco@gmail.com>
2019-12-16 23:50:47 +0100

    first-commit

---
+++ laravel6/app/Http/Controllers/PostController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class PostController extends Controller
+{
+    public function show($slug){
+        
+        $post = DB::table('posts')->where('slug',$slug)->first();
+
+      
+
+        return view('post', [
+            'post'=>$post 
+        ]);
+        
+    }
+}
---
+++ laravel6/routes/web.php
@@ -0,0 +1,43 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('test', function(){
+    $name = request('name');
+    return view('test',[
+        'name' => $name
+    ]);
+});
+
+/*
+Route::get('post/{post}', function($post){
+    $posts = [
+        'my-first-post'=>'Hello, this is my first blog post!',
+        'my-second-post'=>'Now I am getting the hang of this blogging thing.'
+    ];
+
+    if(! array_key_exists($post,$posts)){
+        abort(404,"Sorry, that post was not found");
+    }
+
+    return view('post', [
+        'post'=>$posts[$post] //?? "nothing here yet"
+    ]);
+    
+});
+*/
+
+Route::get('post/{post}','PostController@show');
